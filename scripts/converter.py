6b21fe91810d0bfd0ede464e87dc936a6b361dbe
Igor Baliuk <minefoggiar@gmail.com>
2019-11-15 13:49:51 +0300

    Added frontend

--- scripts/converter.py
+++ scripts/converter.py
@@ -4,19 +4,30 @@ from pathlib import Path
 
 os = platform.system()
 
-def convert_encoding(root_path, initial_encoding, final_encoding):
-    if os == "Windows":
-        iconv = str(Path(f"{root_path}/iconv/bin/iconv.exe"))
-    
-    elif os == "Darwin" or os == "Linux": 
-        iconv = "iconv"
 
+def get_iconv_name(root_path):
+    if os == "Windows":
+        return str(Path(f"{root_path}/iconv/bin/iconv.exe"))
+    elif os == "Darwin" or os == "Linux":
+        return "iconv"
+
+
+def get_available_formats(root_path):
+    try:
+        return subprocess.check_output([get_iconv_name(root_path), "--list"], text=True).split(
+            "//\n")
+    except subprocess.CalledProcessError:
+        raise
+
+
+def convert_encoding(root_path, initial_encoding, final_encoding):
+    iconv = get_iconv_name(root_path)
     encodings_string = f" -f {initial_encoding} -t {final_encoding} "
     input_file_path = str(Path(f"{root_path}/target-files/input.txt"))
     output_sym = " > "
     output_file_path = str(Path(f"{root_path}/target-files/{final_encoding}-converted.txt"))
     cmd = iconv + encodings_string + input_file_path + output_sym + output_file_path
-    
+
     try:
         subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     except subprocess.CalledProcessError:
---
+++ static/script.js
@@ -0,0 +1,47 @@
+$('#file').on('change', function () {
+    var fileName = $(this).val().replace(/C:\\fakepath\\/i, '');
+    $(this).next('.custom-file-label').html(fileName);
+})
+
+$('#btn-file').on('click', function () {
+   $('#file').click();
+})
+
+$('#submit-file').on('click', function () {
+    $('#convert-message-result').remove();
+    let responseLabel = $('<div>', {
+        class: 'alert',
+        id: 'convert-message-result'
+    });
+    $('#main').append(responseLabel);
+
+    if ($('#file').val() === "") {
+        responseLabel.text("You didn't choose a file!");
+        responseLabel.addClass('alert-danger');
+    } else {
+        let formData = new FormData();
+        formData.append('file', $('#file')[0].files[0]);
+
+        fetch('/convert/' + $('#inputGroupSelect :selected').text(), {
+            method: 'POST',
+            body: formData
+        })
+            .then(resp => resp.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                a.download = $('#file').val().replace(/C:\\fakepath\\/i, '');
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+                responseLabel.text("Success!");
+                responseLabel.addClass('alert-success');
+            })
+            .catch(() => {
+                responseLabel.text("Bad request!");
+                responseLabel.addClass('alert-warning');
+            });
+    }
+})
